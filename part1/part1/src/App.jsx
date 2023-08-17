1173ca41c19c65297a0e2825c1b8488f49730e71
pkrzekotowski <piotr.krzekotowski79@gmail.com>
2023-08-17 19:44:02 +0200

    testing out the code

---
+++ part1/part1/src/App.jsx
@@ -0,0 +1,25 @@
+const Hello = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={28+10} />
+      <Hello name={name} age={age} />
+    </>
+  )
+}
+
+export default App
