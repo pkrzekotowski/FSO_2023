bbb0f886f6fc0eac228d6097056e2f880cc67933
pkrzekotowski <piotr.krzekotowski79@gmail.com>
2023-08-18 19:17:44 +0200

    completed exercise 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,4 +1,5 @@
 const Header = (props) => {
+  console.log(props)
   return (
     <>
       <p>{props.course}</p>
@@ -6,12 +7,19 @@ const Header = (props) => {
   )
 }
 
+const Part = (props) => {
+  console.log(props)
+  return (
+    <p>{props.part} {props.exercise}</p>
+  )
+}
+
 const Content = (props) => {
   return (
     <>
-      <p>{props.part1} {props.exercise1}</p>
-      <p>{props.part2} {props.exercise2}</p>
-      <p>{props.part3} {props.exercise3}</p>
+      <Part part={props.part1} exercise={props.exercise1} />
+      <Part part={props.part2} exercise={props.exercise2} />
+      <Part part={props.part3} exercise={props.exercise3} />
     </>
   )
 }
