ddce1f2e682f6bbd1314c03a748ae2bea57d9312
pkrzekotowski <piotr.krzekotowski79@gmail.com>
2023-10-24 16:55:38 +0200

    initial commit

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+
+const Button = ({ onClick, text }) => {
+  return(
+    <div>
+      <button onClick={onClick}>
+        {text}
+      </button>
+    </div>
+  )
+}
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const [selected, setSelected] = useState(0)
+
+  console.log('state before', selected)
+  const handleClick = () => setSelected(Math.floor(Math.random() * 8))
+  console.log('state after', selected)
+
+  return (
+    <div>
+      {anecdotes[selected]}
+      <Button text={'next anecdote'} onClick={handleClick} />
+    </div>
+  )
+}
+
+export default App
